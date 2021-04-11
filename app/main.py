0f9e56cdb641870f7ac180834764008228f1cc54
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2021-04-11 15:20:29 +0300

    initial superuser, admin, shop for develop in try block

--- app/main.py
+++ app/main.py
@@ -26,32 +26,35 @@ async def startup():
     """
     Initialization on startup
     """
-    db = SessionLocal()
-    superuser = crud.user.get_by_phone(db, phone=settings.FIRST_SUPERUSER)
-    if not superuser:
-        user_in = schemas.SuperUserCreate(
-            phone=settings.FIRST_SUPERUSER,
-            password=settings.FIRST_SUPERUSER_PASSWORD,
-            is_superuser=True
-        )
-        crud.user.create(db, obj_in=user_in)
+    try:
+        db = SessionLocal()
+        superuser = crud.user.get_by_phone(db, phone=settings.FIRST_SUPERUSER)
+        if not superuser:
+            user_in = schemas.SuperUserCreate(
+                phone=settings.FIRST_SUPERUSER,
+                password=settings.FIRST_SUPERUSER_PASSWORD,
+                is_superuser=True
+            )
+            crud.user.create(db, obj_in=user_in)
 
-        user_in = schemas.AdminCreate(
-            phone=settings.DEVELOP_ADMIN,
-            password=settings.DEVELOP_ADMIN_PASSWORD,
-            full_name='develop shop admin',
-            position='Владелец',
-            is_staff=False
-        )
-        crud.user.create(db, obj_in=user_in)
+            user_in = schemas.AdminCreate(
+                phone=settings.DEVELOP_ADMIN,
+                password=settings.DEVELOP_ADMIN_PASSWORD,
+                full_name='develop shop admin',
+                position='Владелец',
+                is_staff=False
+            )
+            crud.user.create(db, obj_in=user_in)
 
-        admin = crud.user.get_by_phone(db=db, phone=settings.DEVELOP_ADMIN)
-        shop_in = schemas.ShopCreate(
-            name=settings.DEVELOP_SHOP_NAME,
-            is_active=True,
-            address='Develop shop address'
-        )
-        crud.shop.create_with_owner(db, obj_in=shop_in, owner=admin)
+            admin = crud.user.get_by_phone(db=db, phone=settings.DEVELOP_ADMIN)
+            shop_in = schemas.ShopCreate(
+                name=settings.DEVELOP_SHOP_NAME,
+                is_active=True,
+                address='Develop shop address'
+            )
+            crud.shop.create_with_owner(db, obj_in=shop_in, owner=admin)
+    finally:
+        db.close()
 
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
